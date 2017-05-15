a8ceff991fddb8dbb1059258ded859b010ee6d1c
Pavel Lobashov <shockwavenn@gmail.com>
2017-05-15 17:39:39 +0300

    Differentiate files and folders in `file_list`

--- lib/onlyoffice_github_helper/github_client/file_list.rb
+++ lib/onlyoffice_github_helper/github_client/file_list.rb
@@ -2,9 +2,11 @@ module OnlyofficeGithubHelper
   # Module for working with file list
   module FileList
     def file_list(repo, refs: 'master')
-      Octokit.tree(repo, refs, recursive: true).tree
-             .reject { |elem| elem.type == 'tree' }
-             .map(&:path)
+      Octokit.tree(repo, refs, recursive: true).tree.map do |file|
+        path = file.path
+        path << '/' if file.type == 'tree'
+        path
+      end
     end
   end
 end
